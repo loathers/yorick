bc04d0d8de45abd0ef4fb79476655eea94fa4cd0
Patrick Hulin <patrick.hulin@gmail.com>
2022-01-09 16:22:04 -0500

    Add API server.

---
+++ server/src/index.ts
@@ -0,0 +1,70 @@
+import "core-js/modules/es.object.from-entries";
+import * as kolmafia from "kolmafia";
+import { formFields, getProperty, print, writeln } from "kolmafia";
+
+function json(response: { [index: string]: unknown }): void {
+  writeln(JSON.stringify(response));
+}
+
+// API: x-www-form-urlencoded with "body" field as JSON.
+export function main(): void {
+  const bodyString = formFields().body;
+  if (!bodyString) {
+    json({
+      error:
+        'Request must have a "body" POST field and be x-www-form-urlencoded.',
+    });
+    return;
+  }
+
+  const body = JSON.parse(bodyString);
+  if (!body) {
+    json({
+      error: "Invalid JSON in body field.",
+    });
+    return;
+  }
+
+  const result = {};
+
+  // properties: list of property names
+  // returns object { [name]: value as string }
+  if (body.properties) {
+    const valid = body.properties.filter(
+      (name: unknown) => typeof name === "string"
+    ) as string[];
+
+    Object.assign(result, {
+      properties: Object.fromEntries(
+        valid.map((name) => [name, getProperty(name)])
+      ),
+    });
+  }
+
+  // functions: list of { name, args } objects.
+  // returns object { [name]: result }
+  if (body.functions) {
+    const valid = body.functions.filter(
+      ({ name }: { name?: string; args?: string }) =>
+        typeof name === "string" && name in kolmafia
+    ) as { name: keyof typeof kolmafia; args?: unknown }[];
+
+    Object.assign(result, {
+      functions: Object.fromEntries(
+        valid.map(({ name, args }) => {
+          if (typeof kolmafia[name] !== "function") {
+            return [name, null];
+          }
+          print(name);
+          kolmafia.dump(kolmafia[name]);
+
+          const f = kolmafia[name] as (...args: unknown[]) => unknown;
+
+          return [name, f(...(Array.isArray(args) ? args : []))];
+        })
+      ),
+    });
+  }
+
+  writeln(JSON.stringify(result));
+}
