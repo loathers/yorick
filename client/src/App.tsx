386c59bd32ec07b02ae659527197aa0f59079528
horrible little slime <69secret69email69@gmail.com>
2022-01-17 18:27:40 -0500

    prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,25 +3,25 @@ import QuestSection from "./sections/QuestSection";
 import ResourceSection from "./sections/ResourceSection";
 
 const theme = extendTheme({
-    textStyles: {
-        line: {
-            lineHeight: 1.15,
-        },
+  textStyles: {
+    line: {
+      lineHeight: 1.15,
     },
+  },
 });
 
 function App() {
-    return (
-        <ChakraProvider theme={theme}>
-            <Flex direction='column' align='stretch'>
-                <Heading as='h1' size='xl' alignSelf='center'>
-                    Y💀RICK
-                </Heading>
-                <QuestSection />
-                <ResourceSection />
-            </Flex>
-        </ChakraProvider>
-    );
+  return (
+    <ChakraProvider theme={theme}>
+      <Flex direction="column" align="stretch">
+        <Heading as="h1" size="xl" alignSelf="center">
+          Y💀RICK
+        </Heading>
+        <QuestSection />
+        <ResourceSection />
+      </Flex>
+    </ChakraProvider>
+  );
 }
 
 export default App;
