070afaa8552f298d63e433b2d6352f6882eb7c1f
horrible little slime <69secret69email69@gmail.com>
2022-01-17 13:09:15 -0500

    add level4

--- client/src/sections/QuestSection.tsx
+++ client/src/sections/QuestSection.tsx
@@ -2,12 +2,14 @@ import Section from "../components/Section";
 import Level1 from "./quests/Level1";
 import Level2 from "./quests/Level2";
 import Level3 from "./quests/Level3";
+import Level4 from "./quests/Level4";
 
 const QuestSection = () => (
   <Section name="Quests">
     <Level1 />
     <Level2 />
     <Level3 />
+    <Level4 />
   </Section>
 );
 
