20c3d42049b8be210ebed58fdc6345515d5c241c
Aaron McGuire <aaron@gothicginobili.com>
2022-01-18 10:09:41 -0500

    support for Backup Camera

--- client/src/sections/ResourceSection.tsx
+++ client/src/sections/ResourceSection.tsx
@@ -1,12 +1,14 @@
 import Section from "../components/Section";
 import ColdMedicineCabinet from "./resources/ColdMedicineCabinet";
 import CursedMagnifyingGlass from "./resources/CursedMagnifyingGlass";
+import BackupCamera from "./resources/backupCamera";
 import FreeFights from "./resources/FreeFights";
 import UndergroundFireworksShop from "./resources/UndergroundFireworksShop";
 
 const ResourceSection = () => (
   <Section name="Resources">
     <FreeFights />
+    <BackupCamera />
     <ColdMedicineCabinet />
     <CursedMagnifyingGlass />
     <UndergroundFireworksShop />
