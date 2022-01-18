86274ce1f54762c589b2c4209aa08eae08a5ac9f
Aaron McGuire <aaron@gothicginobili.com>
2022-01-18 10:52:21 -0500

    adding powerful glove support

--- client/src/sections/ResourceSection.tsx
+++ client/src/sections/ResourceSection.tsx
@@ -4,12 +4,14 @@ import CursedMagnifyingGlass from "./resources/CursedMagnifyingGlass";
 import BackupCamera from "./resources/backupCamera";
 import FreeFights from "./resources/FreeFights";
 import UndergroundFireworksShop from "./resources/UndergroundFireworksShop";
+import PowerfulGlove from "./resources/PowerfulGlove";
 
 const ResourceSection = () => (
   <Section name="Resources">
     <FreeFights />
     <BackupCamera />
     <ColdMedicineCabinet />
+    <PowerfulGlove />
     <CursedMagnifyingGlass />
     <UndergroundFireworksShop />
   </Section>
