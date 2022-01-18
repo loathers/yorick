d0b856544963ecad25de2ec4aba97cb2196f3bd6
Aaron McGuire <aaron@gothicginobili.com>
2022-01-18 15:12:27 -0500

    running prettier on everything

--- client/src/sections/ResourceSection.tsx
+++ client/src/sections/ResourceSection.tsx
@@ -1,16 +1,18 @@
 import Section from "../components/Section";
 import ColdMedicineCabinet from "./resources/ColdMedicineCabinet";
 import CursedMagnifyingGlass from "./resources/CursedMagnifyingGlass";
-import BackupCamera from "./resources/backupCamera";
 import FreeFights from "./resources/FreeFights";
 import UndergroundFireworksShop from "./resources/UndergroundFireworksShop";
 import PowerfulGlove from "./resources/PowerfulGlove";
+import BackupCamera from "./resources/BackupCamera";
+import CosmicBowlingBall from "./resources/CosmicBowlingBall";
 
 const ResourceSection = () => (
   <Section name="Resources">
     <FreeFights />
     <BackupCamera />
     <ColdMedicineCabinet />
+    <CosmicBowlingBall />
     <PowerfulGlove />
     <CursedMagnifyingGlass />
     <UndergroundFireworksShop />
