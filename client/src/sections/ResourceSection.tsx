8e5a30651fce84584feb07694acf22a74b7aeb67
horrible little slime <69secret69email69@gmail.com>
2022-01-17 23:14:28 -0500

    export the thing

--- client/src/sections/ResourceSection.tsx
+++ client/src/sections/ResourceSection.tsx
@@ -2,15 +2,17 @@ import Section from "../components/Section";
 import ColdMedicineCabinet from "./resources/ColdMedicineCabinet";
 import CursedMagnifyingGlass from "./resources/CursedMagnifyingGlass";
 import FreeFights from "./resources/FreeFights";
+import IndustrialFireExtinguisher from "./resources/IndustrialFireExtinguisher";
 import UndergroundFireworksShop from "./resources/UndergroundFireworksShop";
 
 const ResourceSection = () => (
-  <Section name="Resources">
-    <FreeFights />
-    <ColdMedicineCabinet />
-    <CursedMagnifyingGlass />
-    <UndergroundFireworksShop />
-  </Section>
+    <Section name='Resources'>
+        <FreeFights />
+        <ColdMedicineCabinet />
+        <CursedMagnifyingGlass />
+        <UndergroundFireworksShop />
+        <IndustrialFireExtinguisher />
+    </Section>
 );
 
 export default ResourceSection;
