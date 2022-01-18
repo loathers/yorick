46209e8e607750b7eba7d2a3879b6884b6225225
Patrick Hulin <patrick.hulin@gmail.com>
2022-01-18 16:49:43 -0500

    Add Level 8 quest.

---
+++ client/src/sections/quests/Level8.tsx
@@ -0,0 +1,95 @@
+import Line from "../../components/Line";
+import QuestTile from "../../components/QuestTile";
+import useCall from "../../hooks/useCall";
+import useGet from "../../hooks/useGet";
+import useHave from "../../hooks/useHave";
+import useNumericModifier from "../../hooks/useNumericModifier";
+import { atStep, Step, useQuestStep } from "../../hooks/useQuest";
+import { $item } from "../../util/makeValue";
+import { commaAnd, plural, truthy } from "../../util/text";
+
+const Level4: React.FC = () => {
+  const step = useQuestStep("questL08Trapper");
+  const goatCheese = useCall.itemAmount($item`goat cheese`) ?? 0;
+  const oreType = useGet("trapperOre") ?? "chrome ore";
+  const ore = useCall.itemAmount($item`${oreType}`) ?? 0;
+
+  const rope = useHave($item`ninja rope`);
+  const crampons = useHave($item`ninja crampons`);
+  const carabiner = useHave($item`ninja carabiner`);
+  const ninjaCount = (rope ? 1 : 0) + (crampons ? 1 : 0) + (carabiner ? 1 : 0);
+
+  const coldRes = useNumericModifier("Cold Resistance") ?? 0;
+
+  const yetiCount = useCall.toLocation("Mist-Shrouded Peak")?.turnsSpent ?? 0;
+
+  if (step === Step.FINISHED) return <></>;
+
+  // TODO: Find image URL.
+  return (
+    <QuestTile
+      header="Trapper"
+      href={atStep(step, [
+        [Step.UNSTARTED, "/council.php"],
+        [Step.STARTED, "/place.php?whichplace=mclargehuge_trapper"],
+        [1, "/place.php?whichplace=mclargehuge"],
+        [2, "/place.php?whichplace=mclargehuge_trapper"],
+        [4, "/place.php?whichplace=mclargehuge_trapper"],
+        [Step.FINISHED, undefined],
+      ])}
+      minLevel={8}
+    >
+      {atStep(step, [
+        [Step.UNSTARTED, <Line>Visit Council to start quest.</Line>],
+        [Step.STARTED, <Line>Visit the Trapper to get your assignment.</Line>],
+        [
+          1,
+          <Line>
+            Acquire {3 - goatCheese} goat cheese and {3 - ore} {oreType}.
+          </Line>,
+        ],
+        [
+          2,
+          ninjaCount < 3 ? (
+            <Line>
+              Stack +combat and adventure for{" "}
+              {plural(
+                3 - ninjaCount,
+                "ninja snowman assassin",
+                "ninja snowmen assassins"
+              )}
+              . Need{" "}
+              {commaAnd(
+                truthy([
+                  rope && "ninja rope",
+                  crampons && "ninja crampons",
+                  carabiner && "ninja carabiner",
+                ])
+              )}
+              .
+            </Line>
+          ) : (
+            <Line>
+              {coldRes >= 5
+                ? "Climb"
+                : `Get 5 cold resistance (+${5 - coldRes}) and climb`}{" "}
+              the Icy Peak.
+            </Line>
+          ),
+        ],
+        [
+          4,
+          <Line>
+            {coldRes >= 5
+              ? "Fight"
+              : `Get 5 cold resistance (+${5 - coldRes}) and fight`}{" "}
+            {yetiCount < 3 ? `${3 - yetiCount} yetis and Groar` : "Groar"}
+          </Line>,
+        ],
+        [5, <Line>Return fur to the Trapper.</Line>],
+      ])}
+    </QuestTile>
+  );
+};
+
+export default Level4;
--- client/src/util/text.ts
+++ client/src/util/text.ts
@@ -7,13 +7,25 @@ export function plural(
   return `${count} ${count === 1 ? description : descriptionPlural}`;
 }
 
-export function commaAnd(values: string[]) {
+export function commaList(values: string[], connector: string) {
   if (values.length === 0) return "none";
   else if (values.length === 1) return values[0];
   else if (values.length === 2) return `${values[0]} and ${values[1]}`;
   else {
-    return `${values.slice(0, -1).join(", ")}, and ${
+    return `${values.slice(0, -1).join(", ")}, ${connector} ${
       values[values.length - 1]
     }`;
   }
+}
+
+export function commaAnd(values: string[]) {
+  return commaList(values, "and");
+}
+
+export function commaOr(values: string[]) {
+  return commaList(values, "or");
+}
+
+export function truthy<T>(values: (T | false)[]): T[] {
+  return values.filter((x) => x) as T[];
 }
