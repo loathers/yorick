e001d42649c30d1b5a90a0d32bdcf26f5d24174f
JamesDowney <44683462+JamesDowney@users.noreply.github.com>
2022-01-19 09:22:18 -0700

    dynamically hide equip link

--- client/src/hooks/useEquipURL.ts
+++ client/src/hooks/useEquipURL.ts
@@ -1,9 +1,12 @@
 import { Placeholder } from "../util/makeValue";
-import { useMyHash, useToInt } from "./useCall";
+import { useHaveEquipped, useMyHash, useToInt } from "./useCall";
 
-export function useEquipURL<T extends "Item">(item: Placeholder<T>): string {
+export function useEquipURL<T extends "Item">(
+  item: Placeholder<T>
+): string | undefined {
   const itemID = useToInt(item);
   const myHash = useMyHash();
+  const itemEquipped = useHaveEquipped(item);
   const equipURL = `http://127.0.0.1:60080/inv_equip.php?pwd=${myHash}&which=2&action=equip&whichitem=${itemID}`;
-  return equipURL;
+  return !itemEquipped ? equipURL : undefined;
 }
