1825bf1d63f24e8af079bc1d3e16cabf8fe3e5d6
JamesDowney <44683462+JamesDowney@users.noreply.github.com>
2022-01-19 15:34:38 -0700

    merge conflict resolved

--- client/src/components/Tile.tsx
+++ client/src/components/Tile.tsx
@@ -1,5 +1,7 @@
 import { Heading, HStack, VStack } from "@chakra-ui/react";
 import React from "react";
+import { Placeholder } from "../util/makeValue";
+import EquipLink from "./EquipLink";
 import MainLink from "./MainLink";
 import TileImage from "./TileImage";
 
@@ -9,7 +11,7 @@ export interface TileProps {
   imageAlt?: string;
   href?: string;
   disabled?: boolean;
-  hide?: boolean;
+  itemToEquip?: Placeholder<"Item">;
 }
 
 const Tile: React.FC<TileProps> = ({
@@ -19,16 +21,15 @@ const Tile: React.FC<TileProps> = ({
   href,
   disabled,
   children,
-  hide,
+  itemToEquip,
 }) => {
-  if (hide) return <></>;
-
   const tile = (
     <HStack px={2} textColor={disabled ? "gray.500" : undefined}>
       <TileImage imageUrl={imageUrl} imageAlt={imageAlt ?? header} />
       <VStack align="stretch" spacing={0.3}>
         <Heading as="h3" size="sm">
           {header}
+          {itemToEquip && <EquipLink itemToEquip={itemToEquip} />}
         </Heading>
         {children}
       </VStack>
