a98fe78841d96902d01c638f488cef4ab17a5a0e
James Downey <44683462+JamesDowney@users.noreply.github.com>
2022-01-19 11:48:15 -0700

    Update client/src/hooks/useEquipURL.ts
    Co-authored-by: Patrick Hulin <patrick.hulin@gmail.com>

--- client/src/hooks/useEquipURL.ts
+++ client/src/hooks/useEquipURL.ts
@@ -7,6 +7,6 @@ export function useEquipURL<T extends "Item">(
   const itemID = useToInt(item);
   const myHash = useMyHash();
   const itemEquipped = useHaveEquipped(item);
-  const equipURL = `http://127.0.0.1:60080/inv_equip.php?pwd=${myHash}&which=2&action=equip&whichitem=${itemID}`;
+  const equipURL = `/inv_equip.php?which=2&action=equip&whichitem=${itemID}`;
   return !itemEquipped ? equipURL : undefined;
 }
