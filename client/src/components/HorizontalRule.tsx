ec152c079f911582c515276cb2461f87604062f8
JamesDowney <44683462+JamesDowney@users.noreply.github.com>
2022-01-20 03:01:58 -0700

    added horizontal rule component

---
+++ client/src/components/HorizontalRule.tsx
@@ -0,0 +1,12 @@
+const Hr = () => (
+  <view
+    style={{
+      marginTop: 4,
+      marginBottom: 4,
+      borderBottomColor: "gray.500",
+      borderBottomWidth: 1,
+    }}
+  />
+);
+
+export default Hr;
