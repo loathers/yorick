8c36d8b5ea538b267812dfa103035ca3d3a56b1d
JamesDowney <44683462+JamesDowney@users.noreply.github.com>
2022-01-21 23:24:49 -0700

    switch with variable equip links

--- client/src/components/DynamicLink.tsx
+++ client/src/components/DynamicLink.tsx
@@ -1,4 +1,12 @@
-import { useCanEquip, useMyHash, useToInt } from "../hooks/useCall";
+import { HStack } from "@chakra-ui/react";
+import {
+  useCanEquip,
+  useMyHash,
+  useToInt,
+  useToSlot,
+  useWeaponHands,
+  useWeaponType,
+} from "../hooks/useCall";
 import { $item, Placeholder } from "../util/makeValue";
 import MainLink from "./MainLink";
 
@@ -12,42 +20,88 @@ interface Props {
 const DynamicLink: React.FC<Props> = ({ linkedContent }) => {
   const myHash = useMyHash() ?? 0;
   const isItem = linkedContent.objectType === "Item";
-  const isFamiliar = linkedContent.objectType === "Familiar";
-  const isSkill = linkedContent.objectType === "Skill";
   const linkID = useToInt(linkedContent) ?? 1;
-  const isEquippable =
-    +isItem * +(useCanEquip($item`${linkID + ""}`) ?? 0) === 1;
+  const linkItem = $item`${linkID.toString()}`;
+  const isEquippable = +isItem * +(useCanEquip(linkItem) ?? 0) === 1;
+  const equipSlot = useToSlot(linkItem);
+  const weaponHands = useWeaponHands(linkItem);
+  const weaponType = useWeaponType(linkItem);
 
-  if (isItem) {
-    return (
-      <MainLink
-        href={
-          isEquippable
-            ? `/inv_equip.php?pwd=${myHash}&which=2&action=equip&whichitem=${linkID}`
-            : `/inv_use.php?pwd=${myHash}&which=3&whichitem=${linkID}`
-        }
-      >
-        {isEquippable ? "[equip]" : "[use]"}
-      </MainLink>
-    );
-  } else if (isFamiliar) {
-    return (
-      <MainLink
-        href={`/familiar.php?&action=newfam&newfam=${linkID}&pwd=${myHash}`}
-      >
-        [take with you]
-      </MainLink>
-    );
-  } else if (isSkill) {
-    return (
-      <MainLink
-        href={`/runskillz.php?action=Skillz&whichskill=${linkID}&targetplayer=0&pwd=${myHash}&quantity=1`}
-      >
-        [cast on self]
-      </MainLink>
-    );
+  switch (linkedContent.objectType) {
+    case "Item":
+      if (equipSlot?.identifierString === "acc1") {
+        return (
+          <HStack spacing={1}>
+            <MainLink
+              href={`/inv_equip.php?pwd=${myHash}&which=2&action=equip&whichitem=${linkID}`}
+            >
+              [equip]
+            </MainLink>
+            <MainLink
+              href={`/inv_equip.php?pwd=${myHash}&which=2&action=equip&whichitem=${linkID}&slot=1`}
+            >
+              [1]
+            </MainLink>
+            <MainLink
+              href={`/inv_equip.php?pwd=${myHash}&which=2&action=equip&whichitem=${linkID}&slot=2`}
+            >
+              [2]
+            </MainLink>
+            <MainLink
+              href={`/inv_equip.php?pwd=${myHash}&which=2&action=equip&whichitem=${linkID}&slot=3`}
+            >
+              [3]
+            </MainLink>
+          </HStack>
+        );
+      } else if (
+        isEquippable &&
+        equipSlot?.identifierString === "weapon" &&
+        weaponHands === 1 &&
+        weaponType?.identifierString === "Muscle"
+      ) {
+        return (
+          <HStack spacing={1}>
+            <MainLink
+              href={`/inv_equip.php?pwd=${myHash}&which=2&action=equip&whichitem=${linkID}`}
+            >
+              [equip (1h)]
+            </MainLink>
+            <MainLink
+              href={`/inv_equip.php?pwd=${myHash}&which=2&action=dualwield&whichitem=${linkID}`}
+            >
+              [offhand]
+            </MainLink>
+          </HStack>
+        );
+      } else {
+        return (
+          <MainLink
+            href={`/inv_equip.php?pwd=${myHash}&which=2&action=equip&whichitem=${linkID}`}
+          >
+            [equip]
+          </MainLink>
+        );
+      }
+    case "Familiar":
+      return (
+        <MainLink
+          href={`/familiar.php?&action=newfam&newfam=${linkID}&pwd=${myHash}`}
+        >
+          [take with you]
+        </MainLink>
+      );
+    case "Skill":
+      return (
+        <MainLink
+          href={`/runskillz.php?action=Skillz&whichskill=${linkID}&targetplayer=0&pwd=${myHash}&quantity=1`}
+        >
+          [cast on self]
+        </MainLink>
+      );
+    default:
+      return <></>;
   }
-  return <></>;
 };
 
 export default DynamicLink;
--- client/src/components/Tile.tsx
+++ client/src/components/Tile.tsx
@@ -36,7 +36,7 @@ const Tile: React.FC<TileProps> = ({
     <HStack px={2} textColor={disabled ? "gray.500" : undefined}>
       <TileImage imageUrl={imageUrl} imageAlt={imageAlt ?? header} />
       <VStack align="stretch" spacing={0.3}>
-        <HStack fontSize="12" spacing={2}>
+        <HStack fontSize="sm" spacing={2}>
           <Heading as="h3" size="sm">
             {header}
           </Heading>
