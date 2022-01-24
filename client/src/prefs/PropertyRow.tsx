c046c69de98f9c78f67d3d3d220dfd3c56f020ca
Patrick Hulin <patrick.hulin@gmail.com>
2022-01-24 16:34:27 -0500

    Further tweak border color.

--- client/src/prefs/PropertyRow.tsx
+++ client/src/prefs/PropertyRow.tsx
@@ -100,6 +100,7 @@ const PropertyRow: React.FC<Props> = ({ property }) => {
             onBlur={handleBlur}
             isInvalid={!valid && value !== ""}
             borderColor={valid ? "green.500" : undefined}
+            focusBorderColor={valid ? "green.500" : undefined}
             _hover={valid ? { borderColor: "green.600" } : undefined}
             size="sm"
             minW="6rem"
