8e9c406c7331c156a4c7f921e65fbef4ca4698dc
DSONE\OB8 <ob8@3ds.com>
2022-02-27 23:15:23 -0500

    Only show predictions

--- client/src/sections/resources/CrystalBall.tsx
+++ client/src/sections/resources/CrystalBall.tsx
@@ -1,62 +1,28 @@
-import { Monster, myLocation } from "kolmafia";
-import { $item, $location, $monsters, get, have } from "libram";
+import { haveEquipped, myHash, toLocation } from "kolmafia";
+import { $item, get, have } from "libram";
 import Line from "../../components/Line";
 import Tile from "../../components/Tile";
 
-// consts to make the map below more readable
-const NOOK = $location`The Defiled Nook`.id;
-const LIBRARY = $location`The Haunted Library`.id;
-const AIRSHIP = $location`The Penultimate Fantasy Airship`.id;
-const TOPIARY_ANIMALS = $monsters`bearpig topiary animal, elephant (meatcar?) topiary animal, spider (duck?) topiary animal`;
-const APARTMENT = $location`The Hidden Apartment Building`.id;
-const OFFICE = $location`The Hidden Office Building`.id;
-const LAUNDRY_ROOM = $location`The Haunted Laundry Room`.id;
-const BOBS = $monsters`Bob Racecar, Racecar Bob`;
-const BARREL = $location`Next to that Barrel with Something Burning in it`.id;
-const REFRIGERATOR = $location`Near an Abandoned Refrigerator`.id;
-const TIRES = $location`Over Where the Old Tires Are`.id;
-const CAR = $location`Out by that Rusted-Out Car`.id;
-
-// Map of good orb monster targets by location id
-const TARGETS: { [key: number]: Monster[] } = {};
-TARGETS[LIBRARY] = $monsters`writing desk, banshee librarian`;
-TARGETS[$location`The Beanbat Chamber`.id] = $monsters`beanbat`;
-TARGETS[$location`The Defiled Niche`.id] = $monsters`dirty old lihc`;
-TARGETS[NOOK] = $monsters`spiny skelelton, toothy sklelton`;
-TARGETS[$location`The Goatlet`.id] = $monsters`dairy goat`;
-TARGETS[$location`Twin Peak`.id] = TOPIARY_ANIMALS;
-TARGETS[AIRSHIP] = $monsters`Quiet Healer`;
-TARGETS[$location`The Hidden Hospital`.id] = $monsters`pygmy witch surgeon`;
-TARGETS[APARTMENT] = $monsters`pygmy shaman, pygmy witch accountant`;
-TARGETS[OFFICE] = $monsters`pygmy witch accountant`;
-TARGETS[$location`The Hidden Bowling Alley`.id] = $monsters`pygmy bowler`;
-TARGETS[$location`The Haunted Wine Cellar`.id] = $monsters`possessed wine rack`;
-TARGETS[LAUNDRY_ROOM] = $monsters`cabinet of Dr. Limpieza`;
-TARGETS[$location`The Red Zeppelin`.id] = $monsters`red butler`;
-TARGETS[$location`Inside the Palindome`.id] = BOBS;
-TARGETS[$location`The Middle Chamber`.id] = $monsters`tomb rat`;
-TARGETS[BARREL] = $monsters`batwinged gremlin`;
-TARGETS[REFRIGERATOR] = $monsters`spider gremlin`;
-TARGETS[TIRES] = $monsters`erudite gremlin`;
-TARGETS[CAR] = $monsters`vegetable gremlin`;
-
-const GOOD_ZONE_IDS = Object.keys(TARGETS).map((id) => parseInt(id));
-
 const CrystalBall = () => {
-  const currentPrediction = get("crystalBallPredictions");
-  const currentLocation = myLocation();
-  const targets = GOOD_ZONE_IDS.includes(currentLocation.id)
-    ? `Look for ${TARGETS[currentLocation.id]}`
-    : "";
+  const currentPrediction = get("crystalBallPredictions")?.split('|').map((prediction) => {
+    const data = prediction.split(':');
+    if (data.length === 3) { // account for async data loading
+      const url = haveEquipped($item`miniature crystal ball`) ? `/adventure.php?snarfblat=${toLocation(data[1]).id}&pwd=${myHash()}` : '';
+      return <Line key={data[1]} href={url}>{data[2]} in {data[1]}</Line>;
+    } else {
+      return '';
+    }
+  });
+
   return (
     <Tile
       header="Miniature Crystal Orb"
       imageUrl="/images/itemimages/famball.gif"
       hide={!have($item`miniature crystal ball`)}
-      linkedContent={$item`miniature crystal ball`}
+      linkedContent={$item`miniature crystal ball`} // TODO: fails to equip if currently equipped by another fam
     >
-      <Line>{currentPrediction}</Line>
-      <Line> {targets} </Line>
+      {!haveEquipped($item`miniature crystal ball`) ? <b>Equip and then find:</b> : ''}
+      {currentPrediction}
     </Tile>
   );
 };
