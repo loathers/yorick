8c4d40519837ae522665cc82ce46f148031fab2a
JamesDowney <44683462+JamesDowney@users.noreply.github.com>
2022-06-04 21:50:18 -0600

    setting up black forest

--- client/src/sections/QuestSection.tsx
+++ client/src/sections/QuestSection.tsx
@@ -1,5 +1,6 @@
 import Section from "../components/Section";
 import Level1 from "./quests/Level1";
+import Level11BlackForest from "./quests/Level11BlackForest";
 import Level2 from "./quests/Level2";
 import Level3 from "./quests/Level3";
 import Level4 from "./quests/Level4";
@@ -18,6 +19,7 @@ const QuestSection = () => (
     <Level6 />
     <Level7 />
     <Level8 />
+    <Level11BlackForest />
   </Section>
 );
 
