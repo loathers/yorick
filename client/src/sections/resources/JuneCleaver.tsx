ef0981724e2788eac3a0b2366245af9bd6ad2995
JamesDowney <44683462+JamesDowney@users.noreply.github.com>
2022-07-10 18:41:58 -0600

    refactor choice map

--- client/src/sections/resources/JuneCleaver.tsx
+++ client/src/sections/resources/JuneCleaver.tsx
@@ -9,67 +9,87 @@ const cleaverQueue = get(`juneCleaverQueue`)
   .split(`,`)
   .map((value) => parseInt(value));
 const skipsRemaining = 5 - parseInt(get(`_juneCleaverSkips`));
-const choiceMap = [
+const cleaverChoiceAdventures = [
   {
     choice: 1467,
     name: "Aunts not Ants",
-    desc: "150 moxie substats|250 muscle substats|30 turns of Ashamed (+10 substats/fight, -20% Mus/Mys/Mox)",
+    option1: "150 moxie substats",
+    option2: "250 muscle substats",
+    option3: "30 turns of Ashamed (+10 substats/fight, -20% Mus/Mys/Mox)",
   },
   {
     choice: 1468,
     name: "Bath Time",
-    desc: "150 muscle substats, gob of wet hair|30 turns of Wholesomely Resolved (15 DR, +2 spooky/stench/sleaze res)|30 turns of Kinda Damp (+3 hot res, +50% init)",
+    option1: "150 muscle substats, gob of wet hair",
+    option2:
+      "30 turns of Wholesomely Resolved (15 DR, +2 spooky/stench/sleaze res)",
+    option3: "30 turns of Kinda Damp (+3 hot res, +50% init)",
   },
   {
     choice: 1469,
     name: "Beware of Aligator",
-    desc: "30 turns of Yapping Pal (+20 ML)|Dad's Brandy (size 1 awesome booze)|1500 meat",
+    option1: "30 turns of Yapping Pal (+20 ML)",
+    option2: "Dad's Brandy (size 1 awesome booze)",
+    option3: "1500 meat",
   },
   {
     choice: 1470,
     name: "Delicious Sprouts",
-    desc: "250 mysticality substats|guilty sprout (size 1 food, red rocket for big stats)|250 muscle substats",
+    option1: "250 mysticality substats",
+    option2: "guilty sprout (size 1 food, red rocket for big stats)",
+    option3: "250 muscle substats",
   },
   {
     choice: 1471,
     name: "Hypnotic Master",
-    desc: "mother's necklace (+3 adv per day, never fumble, 5 free rests)|250 muscle substats|30 turns of of 2-5 random effects",
+    option1: "mother's necklace (+3 adv per day, never fumble, 5 free rests)",
+    option2: "250 muscle substats",
+    option3: "30 turns of of 2-5 random effects",
   },
   {
     choice: 1472,
     name: "Lost and Found",
-    desc: "savings bond (potion, 30 turns of +50% meat)|3 turns of beaten up, 100 muscle substats, 250 meat|250 mysticality substats",
+    option1: "savings bond (potion, 30 turns of +50% meat)",
+    option2: "3 turns of beaten up, 100 muscle substats, 250 meat",
+    option3: "250 mysticality substats",
   },
   {
     choice: 1473,
     name: "Poetic Justice",
-    desc: "250 moxie substats|125 mysticality substats|5 turns of beaten up, 5 adventures",
+    option1: "250 moxie substats",
+    option2: "125 mysticality substats",
+    option3: "5 turns of beaten up, 5 adventures",
   },
   {
     choice: 1474,
     name: "Summer Days",
-    desc: "trampled ticket stub (potion, 30 turns of 5% -com)|fire-roasted lake trout (size 1 good food, 50 turns of +3 cold res, +15 hot damage)|250 moxie substats",
+    option1: "trampled ticket stub (potion, 30 turns of 5% -com)",
+    option2:
+      "fire-roasted lake trout (size 1 good food, 50 turns of +3 cold res, +15 hot damage)",
+    option3: "250 moxie substats",
   },
   {
     choice: 1475,
     name: "Teacher's Pet",
-    desc: "30 turns of Teacher's Pet (+2 sleaze res, 50 DA, 5 DR)|teacher's pen (acc, +3 substats/fight, +2 fam exp)|125 muscle substats",
+    option1: "30 turns of Teacher's Pet (+2 sleaze res, 50 DA, 5 DR)",
+    option2: "teacher's pen (acc, +3 substats/fight, +2 fam exp)",
+    option3: "125 muscle substats",
   },
 ];
 
-const availableChoices = choiceMap
+const availableChoices = cleaverChoiceAdventures
   .filter((entry) => !cleaverQueue.includes(entry.choice))
-  .map(({ desc, name }) => {
+  .map(({ option1, option2, option3, name }) => {
     return (
       <ListItem pl="3">
         <AdviceTooltip
           text={
             <>
               <Line fontWeight="bold">Choices:</Line>
-              <List as="ol" styleType="decimal" pl="3.5">
-                {desc.split("|").map((option) => {
-                  return <ListItem>{option}</ListItem>;
-                })}
+              <List as="ol" styleType="decimal" pl="3">
+                <ListItem>{option1}</ListItem>
+                <ListItem>{option2}</ListItem>
+                <ListItem>{option3}</ListItem>
               </List>
             </>
           }
