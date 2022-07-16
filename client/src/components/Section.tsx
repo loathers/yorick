99cc046ac78cfb132ff5f5c55e8ff5cdd897d7ea
JamesDowney <44683462+JamesDowney@users.noreply.github.com>
2022-07-15 18:45:10 -0600

    initial commit

--- client/src/components/Section.tsx
+++ client/src/components/Section.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { Heading, VStack } from "@chakra-ui/react";
+import { Collapse, Heading, VStack } from "@chakra-ui/react";
 
 interface Props {
   name: string;
 }
 
-const Section: React.FC<Props> = ({ name, children }) => (
-  <VStack spacing={1} align="stretch" mb={1}>
-    <Heading as="h2" size="md" px={2}>
-      {name}
-    </Heading>
-    {children}
-  </VStack>
-);
+const Section: React.FC<Props> = ({ name, children }) => {
+  const [show, setShow] = React.useState(true);
+  const handleToggle = () => setShow(!show);
+
+  const section = (
+    <VStack spacing={1} align="stretch" mb={1}>
+      <Heading as="h2" size="md" px={2} onClick={handleToggle} cursor="pointer">
+        {name}
+      </Heading>
+      <Collapse in={show}>{children}</Collapse>
+    </VStack>
+  );
+
+  return section;
+};
 
 export default Section;
