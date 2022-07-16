65737c9d6f553cda6eea7a59007ff8b7f129fba5
JamesDowney <44683462+JamesDowney@users.noreply.github.com>
2022-07-15 23:52:02 -0600

    initial commit

---
+++ client/src/sections/resources/UnbreakableUmbrella.tsx
@@ -0,0 +1,30 @@
+import { myLevel } from "kolmafia";
+import { $item, get, have } from "libram";
+import Line from "../../components/Line";
+import Tile from "../../components/Tile";
+
+const umbrellaMode = get("umbrellaState");
+
+const UnbreakableUmbrella = () => {
+  return (
+    <Tile
+      header="Unbreakable Umbrella"
+      imageUrl="/images/itemimages/unbrella2.gif"
+      linkedContent={$item`unbreakable umbrella`}
+      linkHide={!have($item`unbreakable umbrella`)}
+    >
+      <Line>Current Mode: {umbrellaMode}</Line>
+      {myLevel() < 13 && umbrellaMode !== "broken" && (
+        <Line>Splaying it will increase ML by 25%</Line>
+      )}
+      {umbrellaMode !== "bucket style" && (
+        <Line>Inverting it will increase item% by 25</Line>
+      )}
+      {umbrellaMode !== "cocoon" && (
+        <Line>Closing yourself inside will increase -combat% by 10</Line>
+      )}
+    </Tile>
+  );
+};
+
+export default UnbreakableUmbrella;
