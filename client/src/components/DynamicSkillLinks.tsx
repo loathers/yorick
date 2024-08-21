58cdffe2434c4d1441292009e1707744b07df910
Patrick Hulin <patrick.hulin@gmail.com>
2024-08-21 17:05:25 -0400

    Fix DynamicSkillLinks.

--- client/src/components/DynamicSkillLinks.tsx
+++ client/src/components/DynamicSkillLinks.tsx
@@ -1,23 +1,18 @@
 import { LinkProps } from "@chakra-ui/react";
-import { useMpCost, useMyHash, useMyMp, useToInt } from "../hooks/useCall";
-import { Placeholder } from "../util/makeValue";
 import HeaderButton from "./HeaderButton";
+import { Skill, mpCost, myHash, myMp, toInt } from "kolmafia";
 
 interface Props extends LinkProps {
-  linkedContent: Placeholder<"Skill">;
+  linkedContent: Skill;
 }
 
 const DynamicLinks: React.FC<Props> = ({ linkedContent, ...props }) => {
-  const myHash = useMyHash() ?? 0;
-  const linkID = useToInt(linkedContent) ?? 1;
-
-  const myMp = useMyMp() ?? 0;
-  const mpCost = useMpCost(linkedContent) ?? 0;
+  const linkID = toInt(linkedContent);
 
   return (
     <HeaderButton
-      href={`/runskillz.php?action=Skillz&whichskill=${linkID}&targetplayer=0&pwd=${myHash}&quantity=1`}
-      disabled={myMp < mpCost}
+      href={`/runskillz.php?action=Skillz&whichskill=${linkID}&targetplayer=0&pwd=${myHash()}&quantity=1`}
+      disabled={myMp() < mpCost(linkedContent)}
       {...props}
     >
       cast
