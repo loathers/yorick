1a0b73905acaea2a632350ec58f1efe8eed37d57
Patrick Hulin <patrick.hulin@gmail.com>
2024-08-26 15:04:32 -0400

    show desert, pyramid tiles.

--- client/src/sections/quests/Level11.tsx
+++ client/src/sections/quests/Level11.tsx
@@ -1,8 +1,10 @@
 import { questFinished } from "../../util/quest";
 import BlackForest from "./level11/BlackForest";
 import Copperhead from "./level11/Copperhead";
+import Desert from "./level11/Desert";
 import HiddenCity from "./level11/HiddenCity";
 import Palindome from "./level11/Palindome";
+import Pyramid from "./level11/Pyramid";
 import Zeppelin from "./level11/Zeppelin";
 
 const Level11 = () => {
@@ -17,6 +19,8 @@ const Level11 = () => {
       <Copperhead />
       <Zeppelin />
       <Palindome />
+      <Desert />
+      <Pyramid />
     </>
   );
 };
