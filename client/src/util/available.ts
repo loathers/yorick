32ad8b04de2f0b5f9a4ec22e4c5d62c9ed97b635
AJ Coppa <aj.coppa@gmail.com>
2024-09-01 16:10:17 -0400

    Include workshed items when checking ownership

--- client/src/util/available.ts
+++ client/src/util/available.ts
@@ -1,8 +1,11 @@
-import { Familiar, isUnrestricted, Item, Skill } from "kolmafia";
+import { Familiar, getWorkshed, isUnrestricted, Item, Skill } from "kolmafia";
 import { have } from "libram";
 
 export function haveUnrestricted(thing: Item | Skill | Familiar) {
   // isUnrestricted has overloads for Item, Skill, Familiar.
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return have(thing) && isUnrestricted(thing as any);
+  return (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    isUnrestricted(thing as any) &&
+    (have(thing) || getWorkshed().name === thing.name)
+  );
 }
