380c4bc3f35b80fa85b014f154fd03805e6b11a1
AJ Coppa <aj.coppa@gmail.com>
2024-09-01 16:17:19 -0400

    Add 2022 resources

--- client/src/sections/ResourceSection.tsx
+++ client/src/sections/ResourceSection.tsx
@@ -9,9 +9,18 @@ import DaylightShavingsHelmet from "./resources/2021/DaylightShavingsHelmet";
 import EmotionChip from "./resources/2021/EmotionChip";
 import IndustrialFireExtinguisher from "./resources/2021/IndustrialFireExtinguisher";
 import UndergroundFireworksShop from "./resources/2021/UndergroundFireworksShop";
+import Autumnaton from "./resources/2022/Autumnaton";
+import CombatLoversLocketTile from "./resources/2022/CombatLoverLocket";
+import Cookbookbat from "./resources/2022/Cookbookbat";
 import CosmicBowlingBall from "./resources/2022/CosmicBowlingBall";
 import CursedMagnifyingGlass from "./resources/2022/CursedMagnifyingGlass";
+import DesignerSweatpants from "./resources/2022/DesignerSweatpants";
+import GreyGoose from "./resources/2022/GreyGoose";
 import JuneCleaver from "./resources/2022/JuneCleaver";
+import JurassicParka from "./resources/2022/JurassicParka";
+import MayDayPackage from "./resources/2022/MaydaySupplyPackage";
+import ModelTrainSet from "./resources/2022/ModelTrainSet";
+import TinyStillsuit from "./resources/2022/TinyStillsuit";
 import UnbreakableUmbrella from "./resources/2022/UnbreakableUmbrella";
 import AprilingBandHelmet from "./resources/2024/AprilingBandHelmet";
 import ChestMimic from "./resources/2024/ChestMimic";
@@ -38,7 +47,16 @@ const ResourceSection = () => (
     <UndergroundFireworksShop />
     <CommerceGhost />
     <IndustrialFireExtinguisher />
+    <CombatLoversLocketTile />
+    <GreyGoose />
+    <MayDayPackage />
     <JuneCleaver />
+    <DesignerSweatpants />
+    <TinyStillsuit />
+    <JurassicParka />
+    <Autumnaton />
+    <Cookbookbat />
+    <ModelTrainSet />
     <UnbreakableUmbrella />
     <AprilingBandHelmet />
     <ChestMimic />
