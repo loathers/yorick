2bf5d81be42a1e536483e7a9408069100bfd23b9
Patrick Hulin <patrick.hulin@gmail.com>
2024-09-01 12:16:54 -0400

    Fix removing nags.

--- client/src/contexts/NagContextProvider.tsx
+++ client/src/contexts/NagContextProvider.tsx
@@ -6,29 +6,6 @@ export interface NagContextProviderProps {
   children?: ReactNode;
 }
 
-// function findIndex<T>(array: T[], value: T, compare: (a: T, b: T) => number) {
-//   let low = 0;
-//   let high = array.length;
-//   while (low < high) {
-//     const mid = (low + high) >>> 1;
-//     if (compare(array[mid], value) > 0) {
-//       high = mid;
-//     } else {
-//       low = mid + 1;
-//     }
-//   }
-//   return low;
-// }
-
-// function insertSorted<T>(
-//   array: T[],
-//   element: T,
-//   compare: (a: T, b: T) => number
-// ) {
-//   const index = findIndex(array, element, compare);
-//   return [...array.slice(0, index), element, ...array.slice(index)];
-// }
-
 const NagContextProvider: React.FC<NagContextProviderProps> = ({
   children,
 }) => {
@@ -36,10 +13,15 @@ const NagContextProvider: React.FC<NagContextProviderProps> = ({
 
   const withNag = useCallback(
     (id: string, priority: number, node: ReactNode) => {
-      // Don't do anything if node is falsey.
-      if (node) {
-        setNags((oldNags) => ({ ...oldNags, [id]: { priority, node } }));
-      }
+      setNags((oldNags) => {
+        if (node) {
+          return { ...oldNags, [id]: { priority, node } };
+        } else {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
+          const { [id]: _, ...rest } = oldNags;
+          return rest;
+        }
+      });
     },
     [setNags],
   );
