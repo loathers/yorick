8e50d44aca8ffe8d7c6b0960b41b88cd4ebc3be0
Patrick Hulin <patrick.hulin@gmail.com>
2024-09-03 15:13:02 -0400

    Add nag for CMG.

--- client/src/sections/resources/2022/CursedMagnifyingGlass.tsx
+++ client/src/sections/resources/2022/CursedMagnifyingGlass.tsx
@@ -1,24 +1,53 @@
-import { $item, get, have } from "libram";
+import { haveEquipped } from "kolmafia";
+import { $item, get } from "libram";
 
 import Line from "../../../components/Line";
 import Tile from "../../../components/Tile";
+import { NagPriority } from "../../../contexts/NagContext";
+import useNag from "../../../hooks/useNag";
+import { haveUnrestricted } from "../../../util/available";
 import { plural } from "../../../util/text";
 
 const CursedMagnifyingGlass = () => {
-  const _voidFreeFights = get("_voidFreeFights");
+  const cursedMagnifyingGlass = $item`cursed magnifying glass`;
+  const haveCmg = haveUnrestricted(cursedMagnifyingGlass);
+  const haveCmgEquipped = haveEquipped(cursedMagnifyingGlass);
+  const voidFreeFights = get("_voidFreeFights");
   const cursedMagnifyingGlassCount = get("cursedMagnifyingGlassCount");
 
   const turnsToVoid = 13 - cursedMagnifyingGlassCount;
+
+  useNag(
+    () => ({
+      priority: NagPriority.HIGH,
+      node: haveCmg && voidFreeFights < 5 && turnsToVoid === 0 && (
+        <Tile linkedContent={cursedMagnifyingGlass}>
+          <Line fontWeight={haveCmgEquipped ? "bold" : undefined}>
+            Void monster fight now!
+          </Line>
+          {!haveCmgEquipped && <Line>Equip cursed magnifying glass.</Line>}
+        </Tile>
+      ),
+    }),
+    [
+      cursedMagnifyingGlass,
+      haveCmg,
+      haveCmgEquipped,
+      turnsToVoid,
+      voidFreeFights,
+    ],
+  );
+
+  if (!haveCmg || voidFreeFights >= 5) return null;
 
   return (
     <Tile
       header="Cursed Magnifying Glass"
       imageUrl="/images/itemimages/cursedmag.gif"
-      hide={!have($item`cursed magnifying glass`) || _voidFreeFights >= 5}
       linkedContent={$item`cursed magnifying glass`}
     >
       <Line>
-        {plural(5 - _voidFreeFights, "free void monster")} remaining today.
+        {plural(5 - voidFreeFights, "free void monster")} remaining today.
       </Line>
       <Line>
         Void monster fight{" "}
