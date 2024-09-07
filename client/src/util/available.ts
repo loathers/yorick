6d4cb85982bdc1dfc0460d0c61cee756950d8c4d
Semenar <Semenuska2010@yandex.ru>
2024-09-07 23:02:46 +0400

    Only check items for workshedability
    If you don't have a workshed item right now and you don't have a specific familiar, the function would return True, since the current workshed name (an empty string) is equal to the familiar name (an empty string). This fix makes it so only items can be haveUnrestricted on the premise of matching names.

--- client/src/util/available.ts
+++ client/src/util/available.ts
@@ -6,6 +6,6 @@ export function haveUnrestricted(thing: Item | Skill | Familiar) {
   return (
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     isUnrestricted(thing as any) &&
-    (have(thing) || getWorkshed().name === thing.name)
+    (have(thing) || (thing instanceof Item && getWorkshed().name === thing.name))
   );
 }
