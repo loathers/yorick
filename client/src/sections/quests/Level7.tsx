4e7675a9deb4e0603b29e96efa44def65e185c94
Semenar <Semenuska2010@yandex.ru>
2024-09-08 05:14:08 +0400

    "Use" link on evil eye tile

--- client/src/sections/quests/Level7.tsx
+++ client/src/sections/quests/Level7.tsx
@@ -73,7 +73,7 @@ const Level7 = () => {
     () => ({
       priority: NagPriority.MID,
       node: evilEyeCount > 0 && nookEvilness > cyrptBossEvilness + 1 && (
-        <Tile header={header} imageUrl="/images/itemimages/zomboeye.gif">
+        <Tile header={header} imageUrl="/images/itemimages/zomboeye.gif" linkedContent={$item`evil eye`}>
           <Line>
             You have {plural(evilEyeCount, "evil eye")}. Use{" "}
             {evilEyeCount === 1 ? "it" : "them"} to reduce Nook evilness by{" "}
