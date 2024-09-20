1998a8c958a4a1cb2ddcdc07694c00641d55510c
Patrick Hulin <patrick.hulin@gmail.com>
2024-09-20 16:18:18 -0400

    Add unix command as comment.

--- client/src/prefs/App.tsx
+++ client/src/prefs/App.tsx
@@ -3,6 +3,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { RefreshContextProvider } from "../contexts/RefreshContext";
 import Layout from "./Layout";
 
+// egrep -Roh '\$item`[^`]*`' src | sed 's/$item`//' | tr -d '`' | sort | uniq | jq --raw-input --slurp 'split("\n") | map(select(. != ""))' > src/prefs/items.json
 // sed -n -E 's/^.* (get|getProperty|questStep)\("([_a-zA-Z0-9]+)"(, .*)?\).*/\3/p' src/{sections,hooks,util}/**/*.tsx | sort | uniq | jq -R . | jq -s . > src/prefs/preferences.json
 
 const App = () => (
