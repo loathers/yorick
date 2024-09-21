33f1ea7cc59de5997c404d39742a17a6d7974a7a
Patrick Hulin <patrick.hulin@gmail.com>
2024-09-21 17:39:11 -0400

    Update pref list.

--- client/src/prefs/App.tsx
+++ client/src/prefs/App.tsx
@@ -5,7 +5,7 @@ import Layout from "./Layout";
 
 // egrep -Roh '\$item`[^`]*`' src | sed 's/$item`//' | tr -d '`' | sort -f | uniq | grep -v '${' | jq --raw-input --slurp 'split("\n") | map(select(. != ""))' > src/prefs/items.json
 // egrep -Roh '\$effect`[^`]*`' src | sed 's/$effect`//' | tr -d '`' | sort -f | uniq | grep -v 'none' | jq --raw-input --slurp 'split("\n") | map(select(. != ""))' > src/prefs/effects.json
-// sed -n -E 's/^.* (get|getProperty|questStep)\("([_a-zA-Z0-9]+)"(, .*)?\).*/\3/p' src/{sections,hooks,util}/**/*.tsx | sort | uniq | jq -R . | jq -s . > src/prefs/preferences.json
+// egrep -Roh '(get|getProperty|questStep|questStarted|questFinished)\("([_a-zA-Z0-9]+)"\)' src | sed -E 's/.*"([_a-zA-Z0-9]+)".*/\1/' | tr -d '`' | sort -f | uniq | jq -R . | jq -s . > src/prefs/preferences.json
 
 const App = () => (
   <ChakraProvider>
