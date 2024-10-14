038caac3127e277dc5e2bd226d0a38d22a955cc3
O.A.F. <>
2024-10-14 17:26:36 +0000

    Build Mafia files for commit babbfa8e6a8f835d736a8ae59e6147b87a5a989a

--- relay/relay_YORICK.js
+++ relay/relay_YORICK.js
@@ -1,11 +1,17 @@
 /* eslint-env commonjs */
 
-const { write } = require("kolmafia");
+const { getRevision, write } = require("kolmafia");
 
 function main() {
-  write(
-    '<html><body><script>window.parent.frames.mainpane.location.href = "/yorick/load.html";</script></body></html>',
-  );
+  if (0 < getRevision() && getRevision() < 28088) {
+    write(
+      "<html><body><h1>Need KoLmafia revision at least 28088 to run YORICK.</h1></body></html>",
+    );
+  } else {
+    write(
+      '<html><body><script>window.parent.frames.mainpane.location.href = "/yorick/load.html";</script></body></html>',
+    );
+  }
 }
 
 module.exports.main = main;
